39d429e1ad8a44c50761f4547a4e977cc242c7cd
avniosmani <avni_osmani@aol.com>
2020-06-19 17:55:30 -0400

    user has ability to enter any channel username

--- youtubeAPI.py
+++ youtubeAPI.py
@@ -1,5 +1,6 @@
 #python3 youtubeAPI.py
 #.bashrc
+#phshockey12
 
 import os
 from googleapiclient.discovery import build
@@ -8,9 +9,11 @@ api_key = os.environ.get('YT_API_KEY')
 
 youtube = build('youtube', 'v3', developerKey=api_key)
 
+channel_username = input("Enter channel username to lookup statisitics: ")
+
 request = youtube.channels().list(
         part='statistics',
-        forUsername='phshockey12'
+        forUsername=channel_username
     )
 
 response = request.execute()
